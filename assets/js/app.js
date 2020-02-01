3ddf4e8cbade0627c55ca1dcb4dc1eb1a395f373
galaxy-coffee <begermisha@gmail.com>
2020-02-01 17:44:36 +0300

    site

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,27 @@ $(document).ready(function(){
         centerMode: true,
         slidesToShow: 3,
         slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1000,
+       /* autoplay: true,*/
+       /* autoplaySpeed: 1000,*/
+responsive: [
+    {
+breakpoint: 1024,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: false,
+        slidesToShow: 1,
+          adaptiveHeight: true,
+          infinite: true,
+      }
+    }
+        ]
     });
 });
